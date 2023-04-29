5f91e018652902749efa00d79737b4b4d984542e
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-04-29 08:10:25 +0600

    news data added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 app.get('/', (req, res) => {
@@ -12,7 +13,11 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) =>{
     res.send(categories);
-})
+});
+
+app.get('/news', (req, res) =>{
+  res.send(news);
+});
 
 app.listen(port, () =>{
     console.log(`Dragon is Running on the port ${port}`)
