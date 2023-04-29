c19c9779bd6f1c4808be974c7d64d98ffeb189f6
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-04-29 17:28:22 +0600

    server complete

--- index.js
+++ index.js
@@ -19,6 +19,23 @@ app.get('/news', (req, res) =>{
   res.send(news);
 });
 
+app.get('/news/:id', (req,res) =>{
+  const id = req.params.id;
+  const selectedNews = news.find(n => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get('/categories/:id', (req, res) =>{
+  const id = req.params.id;
+  if(id == 0){
+    res.send(news)
+  }
+  else{
+    const selectedNews = news.filter(n => n.category_id === id);
+    res.send(selectedNews);
+  }
+})
+
 app.listen(port, () =>{
     console.log(`Dragon is Running on the port ${port}`)
 })
